74805c5afb8a911be74daf74582d88e9b1054d26
Haritha0705 <harithawikramasinha2003@gmail.com>
2025-06-07 09:18:54 +0530

    set up srver comfig

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,1 +1,17 @@
-console.log("Hello World!")
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv/config";
+
+const app = express();
+const PORT = process.env.PORT || "8090";
+app.use(cors());
+app.use(express.json({limit:"20md"}));
+
+app.get("/",(req,res) =>{
+    res.send("<h2>Libriry managemt System API</h2>");
+    next();
+} )
+
+app.listen(PORT,()=>{
+    console.log(`Server is up and runing on port ${PORT}`)
+})
