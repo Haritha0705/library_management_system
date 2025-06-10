e55c8fcf73cd6d20c24871438b81f7ea4aab8114
Haritha0705 <harithawikramasinha2003@gmail.com>
2025-06-10 14:20:01 +0530

    fix bug .env file

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,17 +6,22 @@ import { connect } from "./utils/databaseConnection.js";
 
 const app = express();
 const PORT = process.env.PORT || "8090";
+const HOST = "127.0.0.1"; // Added missing HOST
 
+// Middlewares
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ limit: "20mb" }));
 
-app.get("/", (req, res, next) => {
+// Route
+app.get("/", (req, res) => {
     res.send("<h2>📚 Library Management System API</h2>");
-    next();
 });
 
-app.listen(PORT, () => {
-    logger.info(`🚀 Server is up and running on PORT ${PORT}`);
-    connect();
-});
+// Connect to DB
+connect();
 
+// Start Server
+app.listen(PORT, HOST, () => {
+    logger.info(`🚀 Server is up and running at http://${HOST}:${PORT}`);
+});
--- backend/src/configs/index.js
+++ backend/src/configs/index.js
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-console.log("👉 Loaded MONGODB_URL:", process.env.MONGODB_URL); // Add this
+console.log("👉 Loaded MONGODB_URL:", process.env.MONGODB_URL);
 
 export default {
     DB_CONNECTION_STRING: process.env.MONGODB_URL,
