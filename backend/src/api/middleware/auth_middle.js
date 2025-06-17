122d9cf39111188e18a91bfe3a522286d28c79b7
Haritha0705 <harithawikramasinha2003@gmail.com>
2025-06-17 21:12:51 +0530

    delete previous backend folder

--- backend/src/api/middleware/auth_middle.js
+++
@@ -1,7 +0,0 @@
-const authenticate = (req, res, next) => {
-    console.log(req.isAuthenticated());
-    if (req.isAuthenticated()) return next();
-    res.send("<h4>User is not authenticated on the server 👎🏽</h4>");
-};
-
-export { authenticate };
