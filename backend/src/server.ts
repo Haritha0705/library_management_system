85b494f03c0fbe74025b5a319011f8e29e51408a
Haritha0705 <harithawikramasinha2003@gmail.com>
2025-06-18 19:44:55 +0530

    add new files in route folder

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,16 +1,28 @@
 import express,{Request,Response} from 'express';
-import dotenv from "dotenv"
+import dotenv from "dotenv";
+//import cors from "cors";
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT ;
+const PORT = process.env.PORT || 8070;
 
+// middleware
+// app.use(cors({
+//     origin:"http://localhost:5173"
+// }));
+app.use(express.json());
 
-app.get('/', (_:Request, res:Response) => {
+// routes
+app.get('/', (_req: Request, res: Response) => {
     res.send('Hello, TypeScript + Node.js + Express!');
 });
 
+app.use("/api/books",bookRoutes)
+app.use("/api/users",usersRoutes)
+app.use("/api/loans",loansRoutes)
+
+// start server
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
