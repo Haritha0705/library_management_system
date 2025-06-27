b854de5d2e50a11659ca4f22c364ff2042c7c540
Haritha Wikramasinha <harithawikramasinha2003@gmail.com>
2025-06-27 16:34:43 +0530

    add routes in react router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Profile from "./Pages/Profile";
+import React from "react";
 
-const App = () => {
-  return (
-      <>
-          <h1 className="text-3xl font-bold underline">
-              Hello world!
-          </h1>
-      </>
-  )
-}
+const App: React.FC = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/profile" element={<Profile />} />
+            </Routes>
+        </Router>
+    );
+};
 
-export default App
+export default App;
