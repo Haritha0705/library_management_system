5d8eadc82eac3314d14bfc826c3761a62093113b
Haritha Wikramasinha <harithawikramasinha2003@gmail.com>
2025-08-20 09:17:08 +0530

    top book fetching book data and slice 5 book in random

--- frontend/src/Components/Landing Page/TopBooks.tsx
+++ frontend/src/Components/Landing Page/TopBooks.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import { useNavigate } from "react-router-dom";
 import {  Star } from "lucide-react";
-import {books} from "../../assets/assets.ts";
+import {AppContext} from "../../Context/AppContext.tsx";
 
 const TopBooks: React.FC = () => {
     const navigate = useNavigate();
+
+    const adminContext = useContext(AppContext);
+
+    if (!adminContext) return null
+
+    const { books } = adminContext;
 
     return (
         <div className="flex flex-col items-center gap-6 text-gray-900 my-16 px-4 md:px-10">
@@ -14,7 +20,7 @@ const TopBooks: React.FC = () => {
             </p>
 
             <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6 pt-6">
-                {books.slice(0, 5).map((item, index) => (
+                {[...books].sort(()=>Math.random()-0.5).slice(0, 5).map((item, index) => (
                     <div
                         key={index}
                         onClick={() => navigate(`/order/${item._id}`)}
@@ -22,7 +28,7 @@ const TopBooks: React.FC = () => {
                     >
                         <img
                             src={item.image}
-                            alt={item.name}
+                            alt={item.title}
                             className="w-full h-96 object-cover bg-blue-50"
                         />
                         <div className="p-4 flex flex-col items-start gap-2">
@@ -34,7 +40,7 @@ const TopBooks: React.FC = () => {
                                 <p className="text-xs text-gray-500">140 Reviews</p>
                             </div>
 
-                            <p className="text-lg font-medium">{item.name}</p>
+                            <p className="text-lg font-medium">{item.title}</p>
                             <p className="text-gray-500 text-sm">{item.category}</p>
                         </div>
                     </div>
